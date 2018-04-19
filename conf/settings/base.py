3badba6c6daffc4517c7dbbe6ecb5f531b03aa37
cionman <topspin1278@naver.com>
2018-04-19 23:18:56 +0900

    - Github provider 설정 추가

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -148,6 +148,14 @@ SOCIALACCOUNT_PROVIDERS = {
         'AUTH_PARAMS': {
             'access_type': 'online',
         }
+    },
+    'github': {
+        'SCOPE': [
+            'user',
+            'user:email',
+            'repo',
+            'read:org',
+        ],
     }
 }
 
