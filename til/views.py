4f230172891edb463e79311c89b312481b321030
cionman <topspin1278@naver.com>
2018-04-19 23:29:41 +0900

    - til 화면 권한 지정 및 user 별 til 화면 보이게 수정

--- til/views.py
+++ til/views.py
@@ -1,16 +1,22 @@
 from datetime import date
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from til.forms import TilForm
 from til.models import Til
 
 
+@login_required
 def til(request):
     if request.method == 'POST':
         form = TilForm(request.POST)
         if form.is_valid():
-            til = Til(**form.cleaned_data)
+            til = Til(
+                user_id=request.user.id
+                , user_name=request.user.username
+                , user_email=request.user.email
+                , **form.cleaned_data)
             til.save()
             return redirect('til:til')
     else:
@@ -18,5 +24,5 @@ def til(request):
     return render(request, 'til/til.html', {
         'form': form
         , 'now': date.today()
-        , 'til_list': Til.objects.all()
+        , 'til_list': Til.objects.filter(user_id=request.user.id)
     })
