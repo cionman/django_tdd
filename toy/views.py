cfc2af82c2f3dbea1697c929f95db78393ca1adb
cionman <topspin1278@naver.com>
2018-04-19 22:25:42 +0900

    - toy 화면 권한 지정

--- toy/views.py
+++ toy/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
 
 from toy.forms import ToyForm
 from toy.models import Toy
 
 
+@user_passes_test(lambda user: user.is_authenticated and user.is_superuser)
 def create_toy(request):
     if request.method == 'POST':
         form = ToyForm(request.POST, request.FILES)
