63e8fcc28e2b9e1cf9c00756d0177a1a10ec693e
cionman <topspin1278@naver.com>
2018-04-24 22:43:58 +0900

    - codelab constants 모듈 생성

---
+++ codelab/constants.py
@@ -0,0 +1,20 @@
+
+class Constant:
+    TEST_CODELAB_DATA_TITLE = "First Codelab Title"
+    TEST_CODELAB_DATA_DESC = "First Codelab Desc"
+    TEST_CODELAB_DETAIL_DATA_TITLE = "First Codelab Detail Title"
+    TEST_CODELAB_DETAIL_DATA_CONTENTS = "First Codelab Detail Contents"
+    TEST_CODELAB_DETAIL_DATA_SLUG = "first-slug"
+    TEST_UPDATE_CODELAB_DATA_TITLE = "Update Codelab Title"
+    TEST_UPDATE_CODELAB_DATA_DESC = "Update Codelab DESC"
+    TEST_UPDATE_CODELAB_DATA_CONTENTS = "Update Codelab Contents"
+    TEST_UPDATE_CODELAB_DATA_SLUG = "update-slug"
+
+
+    CODELAB_LIST_PAGE_TITLE = "코드랩 리스트"
+    CODELAB_PAGE_TITLE = "코드랩"
+    CODELAB_DETAIL_PAGE_TITLE = "코드랩 상세 페이지"
+    CODELAB_CREATE_PAGE_TITLE = "새 코드랩 작성"
+    CODELAB_DETAIL_CREATE_PAGE_TITLE = "새 코드랩 상세 작성"
+    CODELAB_UPDATE_PAGE_TITLE = "코드랩 수정"
+    CODELAB_UPDATE_DETAIL_PAGE_TITLE = "코드랩 상세 수정"
