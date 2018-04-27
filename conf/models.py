822c23a8f6222598546af15f3190d95c58a664eb
cionman <topspin1278@naver.com>
2018-04-27 14:16:22 +0900

    - 각테이블 user_id 필드 Integer로 변경

--- conf/models.py
+++ conf/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CommonInfo(models.Model):
     """ 공통 모델 정보 """
 
-    user_id = models.CharField(max_length=50)
+    user_id = models.IntegerField()
     user_name = models.CharField(max_length=50)
     user_email = models.EmailField(max_length=50)
     idate = models.DateTimeField(auto_now_add=True)
