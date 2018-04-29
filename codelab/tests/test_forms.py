6cc71695bb2f6525dd6d9a5e767e97f9be48980e
cionman <topspin1278@naver.com>
2018-04-29 23:49:14 +0900

    - codelab form Test 코드 작성

--- codelab/tests/test_forms.py
+++ codelab/tests/test_forms.py
@@ -10,8 +10,22 @@ class CodelabFormTest(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        if not User.objects.filter(username=Constant.CREDENTIALS["username"]).count():
+        if not User.objects.filter(
+                username=Constant.CREDENTIALS["username"]).count():
             cls.user = User.objects.create_user(**Constant.CREDENTIALS)
             Profile.objects.create(user=cls.user, is_writer=True,
                                    is_admin=False)
         super().setUpClass()
+
+    def test_codelab_form(self):
+        form = CodelabForm(user=self.user)
+        html = form.as_p()
+        self.assertIn(
+            '<input type="text" name="title" maxlength="100" required id="id_title" />'
+            , html)
+        self.assertIn(
+            '<input type="file" name="image" required id="id_image" /> <span class="helptext">대표이미지를 선택해주세요.</span>'
+            , html)
+        self.assertIn('<input type="text" name="desc" maxlength="200" required id="id_desc" />', html)
+        self.assertIn('<input type="checkbox" name="isview" id="id_isview" checked />', html)
+        self.assertIn('<select name="category" required id="id_category">', html)
